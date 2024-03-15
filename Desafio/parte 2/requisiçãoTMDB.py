99b1e4e29ec8690e06a00204c92ca35403761848
wesleyruanwr <119066770+wesleyruanwr@users.noreply.github.com>
2024-03-15 15:06:41 -0300

    Requisição TMDB

--- Desafio/parte 2/requisiçãoTMDB.py
+++ Desafio/parte 2/requisiçãoTMDB.py
@@ -18,14 +18,14 @@ def lambda_handler(event, context):
     data_tmdb = response.json()
     json_data_tmdb = json.dumps(data_tmdb, indent=4)
     
-    s3_resource = boto3.resource('s3', 
+    s3 = boto3.resource('s3', 
                             aws_access_key_id = '',
                             aws_secret_access_key = '',
                             aws_session_token = '')
     
     if response.status_code == 200:
         print('arquivo enviado')
-        s3_resource.Object(bucket_name, aws_file_key).put(Body=json_data_tmdb)
+        s3.Object(bucket_name, aws_file_key).put(Body=json_data_tmdb)
     
     else:
         print('Falha!', response.status_code)
