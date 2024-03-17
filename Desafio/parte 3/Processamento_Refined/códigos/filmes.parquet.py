29aeb2f31e46edd5b1c080df436d97e056b31a4c
wesleyruanwr <119066770+wesleyruanwr@users.noreply.github.com>
2024-03-17 10:42:27 -0300

    Processamento Refined

--- Desafio/parte 3/Processamento_Refined/códigos/filmes.parquet.py
+++ Desafio/parte 3/Processamento_Refined/códigos/filmes.parquet.py
@@ -1,44 +1,42 @@
 #LEMBRANDO QUE FIZ PELO GOOGLE COLAB POR PROBLEMAS DE PERMISSÃO NA AWS
 #COMENTEI LINHAS DE CÓDIGO QUE FICARIAM ERRADAS AQUI NO VSCODE APENAS APRA ORGANIZAÇÃO
 
+#ESTA DIFERENTE DO PRINT PORQUE DURANTE O USO DO QUICKSIGHT PRECISEI FAZER MUDANCAS
+
 
 #!pip install boto3 pandas pyarrow
 
 import boto3
 import pandas as pd
-#import pyarrow as pa
-#import pyarrow.parquet as pq
-from io import BytesIO, StringIO
+from io import BytesIO
 
-AWS_ACCESS_KEY_ID=''
-AWS_SECRET_ACCESS_KEY=''
-AWS_SESSION_TOKEN=''
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+AWS_SESSION_TOKEN = ""
 
-s3 = boto3.client('s3',
-                  aws_access_key_id = AWS_ACCESS_KEY_ID,
-                  aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-                  aws_session_token = AWS_SESSION_TOKEN
-)
+s3_client = boto3.client('s3',
+                         aws_access_key_id=AWS_ACCESS_KEY_ID,
+                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                         aws_session_token=AWS_SESSION_TOKEN)
 
-bucket_name = 'desafiowr'
-file_key = 'Trusted/ArquivosCSV/movies_processed.csv'
+bucket_name = "desafiowr"
+parquet_file_key = "Trusted/movies/movies_processed.parquet"
 
-response = s3.get_object(Bucket=bucket_name, Key=file_key)
-csv_str = response['Body'].read().decode('utf-8')
+s3_object = s3_client.get_object(Bucket=bucket_name, Key=parquet_file_key)
+parquet_data = s3_object['Body'].read()
 
-df = pd.read_csv(StringIO(csv_str), sep='|')
+df = pd.read_parquet(BytesIO(parquet_data))
 
-colunas_ficam = ['id', 'tituloPincipal', 'anoLancamento', 'tempoMinutos', 'genero', 'notaMedia', 'numeroVotos']
+colunas_remover = ["id", "tituloOriginal", "generoArtista", "personagem", "nomeArtista", "anoNascimento", "anoFalecimento", "profissao", "titulosMaisConhecidos"]
+df = df.drop(columns=colunas_remover)
 
-df = df[colunas_ficam]
+df.drop_duplicates(subset='tituloPrincipal', keep='first', inplace=True)
 
-df['anoLancamento'] = pd.to_numeric(df['anoLancamento'], errors='coerce')
+print(df.head())
 
-parquet_buffer = BytesIO()
-#table = pa.Table.from_pandas(df)
-#pq.write_table(table, parquet_buffer)
+s3_key = "s3/Refined/filmes/" 
 
-novo_parquet = 'Refined/arquivo.parquet'
-
-parquet_buffer.seek(0)
-s3.put_object(Bucket=bucket_name, Key=novo_parquet, Body=parquet_buffer.getvalue())
+buffer = BytesIO()
+df.to_parquet(buffer)
+buffer.seek(0)
+s3_client.put_object(Body=buffer, Bucket=bucket_name, Key=s3_key)
